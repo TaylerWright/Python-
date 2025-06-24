6f5cacd4f6352a089583f337fa899a6be3bf4467
TaylerWright <44408920+TaylerWright@users.noreply.github.com>
2025-06-24 15:14:42 -0400

    Create .githTemp.py

---
+++ .githTemp.py
@@ -0,0 +1,9 @@
+# Create a temperature.py program that converts a number from Fahrenheit (°F) to Celsius (°C).
+#Google the current temperature of D.C (where Codédex is based) in °F.
+
+
+temp_f = 103
+temp_c = (temp_f - 32) / 1.8
+
+print(temp_c)
+
